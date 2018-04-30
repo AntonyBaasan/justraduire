a24512f9ab4c62ad4254d1db940e908e732a8ff4
Antony Baasandorj <antony.baasan@gmail.com>
2018-04-30 17:14:52 -0400

    add result panel:

--- src/main/webapp/app/translator/translator.component.ts
+++ src/main/webapp/app/translator/translator.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'jhi-translator',
   templateUrl: './translator.component.html',
-  styles: []
+  styleUrls: [ 'translator.css']
 })
 export class TranslatorComponent implements OnInit {
 
